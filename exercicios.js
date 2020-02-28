9286da0458b31a608e6ba82b63f43c6ca70d26c1
Vinicius Dos Santos Paixão <vinicius.ds.paixao@gmail.com>
2020-02-28 16:23:36 -0300

    exercicios modulo2

--- exercicios.js
+++ exercicios.js
@@ -2,18 +2,43 @@
 // Crie um botão que ao ser clicado cria um novo elemento em tela com a forma de um quadrado
 // vermelho com 100px de altura e largura. Sempre que o botão for clicado um novo quadrado deve
 // aparecer na tela.
+var  botao = document.querySelector('button.button')
+console.log(botao)
+botao.onclick = function() {
+    console.log(onclick)
+    var square = document.createElement('div')
+    square.setAttribute('class','square')
+    square.style.width = "100px"
+    square.style.height = "100px"
+    square.style.backgroundColor = "#f00"
+
+    var app = document.querySelector('div.app')
+    app.appendChild(square)
+}
+
+
+
 // 2º exercício
 // Utilizando o resultado do primeiro desafio, toda vez que o usuário passar o mouse por cima de
 // algum quadrado troque sua cor para uma cor aleatória gerada pela função abaixo:
+var quadrado = document.querySelector('div.square')
+console.log(quadrado)
+quadrado.onmouseenter = function() {
+    return alert("funcionando")
+}
+
 function getRandomColor() {
- var letters = "0123456789ABCDEF";
- var color = "#";
- for (var i = 0; i < 6; i++) {
- color += letters[Math.floor(Math.random() * 16)];
- }
- return color;
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+
+    for (var i = 0; i < 6; i++) {
+         color += letters[Math.floor(Math.random() * 16)];
+    }
+    
+    return color;
 }
 var newColor = getRandomColor(); // #E943F0
+
 // 3º exercício
 // A partir do seguinte vetor:
 // var nomes = ["Diego", "Gabriel", "Lucas"];
